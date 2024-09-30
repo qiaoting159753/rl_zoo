a93312190a63d7c7cb4620db2cd530d4b39b4198
tony <tqia574@aucklanduni.ac.nz>
2024-09-30 18:00:11 +1300

    Test Gaussian Process

--- main_ensemble.py
+++ main_ensemble.py
@@ -17,11 +17,8 @@ def main():
     env_task = data["task"]
     # MBRL
     model_G = data["model_G"]
-    horizon = data["horizon"]
-    branch_factor = data["branch_factor"]
     # Training
     G = data["G"]
-    loop_name = data["loop_name"]
     batch_size = data["batch_size"]
     episode_steps = data["episode_steps"]
     evaluate_interval = data["evaluate_interval"]
@@ -37,14 +34,13 @@ def main():
     for sigma in sigmas:
         for ratio in ratios:
             sub_directory = agent_name + "_" + env_domain + "_" + env_task + "_" + str(sigma) + "_" + str(ratio)
-            # parent_dir = "/root/rl_zoo_data/"
-            parent_dir = "statistics/"
+            parent_dir = "/root/rl_zoo_data/"
+            # parent_dir = "statistics/"
             if not os.path.exists(parent_dir):
                 os.mkdir(parent_dir)
             directory = os.path.join(parent_dir, sub_directory)
             if not os.path.exists(directory):
                 os.mkdir(directory)
-
             for seed in seeds:
                 # Random Seed
                 set_seed(seed)
@@ -53,55 +49,23 @@ def main():
                 env.set_seed(seed)
                 # Agent
                 # CHANGE THIS PART WHEN ON SCHOOL #
-                trainer = None
-                if loop_name == "MFRL":
-                    trainer = MFRL_Trainer(env=env,
-                                           agent_name=agent_name,
-                                           evaluate_interval=evaluate_interval,
-                                           random_goal=random_goal,
-                                           device=device,
-                                           G=G,
-                                           batch_size=batch_size,
-                                           episode_steps=episode_steps,
-                                           maximum_steps=maximum_steps,
-                                           generate_results=True,
-                                           seed=seed,
-                                           directory=directory)
-                if loop_name == "MBRL":
-                    trainer = MBRL_Trainer(env=env,
-                                           agent_name=agent_name,
-                                           random_goal=random_goal,
-                                           device=device,
-                                           G=G,
-                                           model_G=model_G,
-                                           batch_size=batch_size,
-                                           horizon=horizon,
-                                           branch_factor=branch_factor,
-                                           episode_steps=episode_steps,
-                                           maximum_steps=maximum_steps,
-                                           seed=seed,
-                                           directory=directory,
-                                           generate_results=True,
-                                           evaluate_interval=evaluate_interval)
-
-                if loop_name == "World_Model":
-                    on_policy = data["on_policy"]
-                    trainer = World_Model_Trainer(env=env,
-                                                  evaluate_interval=evaluate_interval,
-                                                  world_model_name=agent_name,
-                                                  random_goal=random_goal,
-                                                  device=device,
-                                                  on_policy=on_policy,
-                                                  G=G,
-                                                  model_G=model_G,
-                                                  batch_size=batch_size,
-                                                  episode_steps=episode_steps,
-                                                  maximum_steps=maximum_steps,
-                                                  generate_results=True,
-                                                  seed=seed,
-                                                  directory=directory,
-                                                  ratio=ratio,
-                                                  sigma=sigma)
+                on_policy = data["on_policy"]
+                trainer = World_Model_Trainer(env=env,
+                                              evaluate_interval=evaluate_interval,
+                                              world_model_name=agent_name,
+                                              random_goal=random_goal,
+                                              device=device,
+                                              on_policy=on_policy,
+                                              G=G,
+                                              model_G=model_G,
+                                              batch_size=batch_size,
+                                              episode_steps=episode_steps,
+                                              maximum_steps=maximum_steps,
+                                              generate_results=True,
+                                              seed=seed,
+                                              directory=directory,
+                                              ratio=ratio,
+                                              sigma=sigma)
                 trainer.train()
                 # try:
                 #     trainer.train()
