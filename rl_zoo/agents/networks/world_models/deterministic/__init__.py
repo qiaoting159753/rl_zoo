2abf5c92755b30cfd08e1c7288dc2e1bce2fedac
tony <tqia574@aucklanduni.ac.nz>
2024-11-12 08:47:48 +1300

    comment gp model

--- rl_zoo/agents/networks/world_models/deterministic/__init__.py
+++ rl_zoo/agents/networks/world_models/deterministic/__init__.py
@@ -4,4 +4,3 @@ from .world_single_sas_rwd import Single_PNN
 from .z_world_nf_nvp import NVP_World_Model
 from .z_world_nf_conditional_nvp import Conditional_NVP_World_Model
 from .z_world_nf_nvp import NVP_Flows
-from .z_world_gaussian_process import Gaussian_Process_World_Model
--- rl_zoo/agents/networks/world_models/deterministic/z_world_gaussian_process.py
+++ rl_zoo/agents/networks/world_models/deterministic/z_world_gaussian_process.py
@@ -1,139 +1,139 @@
-import torch
-from rl_zoo.agents.networks.world_models import World_Model
-import torch.nn.functional as F
-from torch import optim
-import pyro
-import pyro.contrib.gp as gp
-
-
-class Gaussian_Process_World_Model(World_Model):
-    def __init__(self, observation_size: int, num_actions: int, device: str,
-                 l_r: float,
-                 noise: float = 0.2,
-                 hidden_size: int = 128,
-                 train_iter: int = 10):
-        super().__init__(observation_size, num_actions, l_r, device, hidden_size)
-        self.gpr = None
-        self.statistics = None
-        self.observation_size = observation_size
-        self.num_actions = num_actions
-        self.l_r = l_r
-        self.device = device
-        self.noise = noise
-        self.train_iter = train_iter
-        self.kernel = gp.kernels.RBF(input_dim=observation_size + num_actions).to(self.device)
-        self.reward_model = Probabilistic_SAS_Reward(observation_size=observation_size, num_actions=num_actions,
-                                                     hidden_size=hidden_size)
-        self.reward_optimizers = optim.Adam(self.reward_model.parameters(), lr=l_r)
-        self.reward_model.to(self.device)
-
-    def set_statistics(self, statistics: dict) -> None:
-        """
-        Update all statistics for normalization for all world models and the
-        ensemble itself.
-        :param (Dictionary) statistics:
-        """
-        return
-
-    def train_world(
-            self,
-            states: list,
-            actions: list,
-            next_states: list,
-    ) -> None:
-        """
-        For nothing.
-        :param states:
-        :param actions:
-        :param next_states:
-        :return:
-        """
-        return
-
-    def train_world_all(
-            self,
-            states: torch.Tensor,
-            actions: torch.Tensor,
-            next_states: torch.Tensor,
-    ) -> None:
-        """
-        Train the dynamic of world model.
-        :param states:
-        :param actions:
-        :param next_states:
-        """
-        y_y = next_states.T
-        x_x = torch.cat((states, actions), dim=1)
-        gpr = gp.models.GPRegression(x_x, y_y, self.kernel, noise=torch.tensor(self.noise), jitter=0.0001).to(self.device)
-        optimizer = torch.optim.Adam(gpr.parameters(), lr=self.l_r)
-        loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
-        for _ in range(self.train_iter):
-            optimizer.zero_grad()
-            loss = loss_fn(gpr.model, gpr.guide)
-            loss.backward()
-            optimizer.step()
-        self.gpr = gpr
-
-    def pred_next_states(
-            self, observation: torch.Tensor, actions: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        :param observation:
-        :param actions:
-        """
-        # sample = 3
-        # preds = []
-        x = torch.cat((observation, actions), dim=1)
-        preds, _ = self.gpr(x, full_cov=True)
-        preds = preds.T
-        preds += observation
-        # preds = torch.mean(preds, dim=0).unsqueeze(dim=0)
-        return preds, None, None, None
-
-    def estimate_uncertainty(
-            self, observation: torch.Tensor, actions: torch.Tensor
-    ) -> tuple[float, float]:
-        """
-        Estimate next state uncertainty and reward uncertainty.
-
-        :param observation:
-        :param actions:
-        :return:
-        """
-        x = torch.cat((observation, actions), dim=1)
-        _, covs = self.gpr(x, full_cov=True)
-        uncet = torch.sum(torch.squeeze(covs)).detach().cpu().numpy()
-        return uncet, 0.0
-
-    def train_reward(
-            self,
-            states: torch.Tensor,
-            actions: torch.Tensor,
-            next_states: torch.Tensor,
-            rewards: torch.Tensor,
-    ) -> None:
-        """
-        Train the reward prediction with or without world model dynamics.
-
-        :param states:
-        :param actions:
-        :param next_states:
-        :param rewards:
-        """
-        self.reward_optimizers.zero_grad()
-        rwd_mean, rwd_var = self.reward_model.forward(states, actions, next_states)
-        reward_loss = F.gaussian_nll_loss(input=rwd_mean, target=rewards, var=rwd_var).mean()
-        reward_loss.backward()
-        self.reward_optimizers.step()
-
-    def pred_rewards(self, observation: torch.Tensor, action: torch.Tensor, next_observation: torch.Tensor
-                     ):
-        """
-        Predict reward based on SAS
-        :param observation:
-        :param action:
-        :param next_observation:
-        :return:
-        """
-        pred_reward, reward_var = self.reward_model.forward(observation, action, next_observation)
-        return pred_reward, None, reward_var
+# import torch
+# from rl_zoo.agents.networks.world_models import World_Model
+# import torch.nn.functional as F
+# from torch import optim
+# import pyro
+# import pyro.contrib.gp as gp
+#
+#
+# class Gaussian_Process_World_Model(World_Model):
+#     def __init__(self, observation_size: int, num_actions: int, device: str,
+#                  l_r: float,
+#                  noise: float = 0.2,
+#                  hidden_size: int = 128,
+#                  train_iter: int = 10):
+#         super().__init__(observation_size, num_actions, l_r, device, hidden_size)
+#         self.gpr = None
+#         self.statistics = None
+#         self.observation_size = observation_size
+#         self.num_actions = num_actions
+#         self.l_r = l_r
+#         self.device = device
+#         self.noise = noise
+#         self.train_iter = train_iter
+#         self.kernel = gp.kernels.RBF(input_dim=observation_size + num_actions).to(self.device)
+#         self.reward_model = Probabilistic_SAS_Reward(observation_size=observation_size, num_actions=num_actions,
+#                                                      hidden_size=hidden_size)
+#         self.reward_optimizers = optim.Adam(self.reward_model.parameters(), lr=l_r)
+#         self.reward_model.to(self.device)
+#
+#     def set_statistics(self, statistics: dict) -> None:
+#         """
+#         Update all statistics for normalization for all world models and the
+#         ensemble itself.
+#         :param (Dictionary) statistics:
+#         """
+#         return
+#
+#     def train_world(
+#             self,
+#             states: list,
+#             actions: list,
+#             next_states: list,
+#     ) -> None:
+#         """
+#         For nothing.
+#         :param states:
+#         :param actions:
+#         :param next_states:
+#         :return:
+#         """
+#         return
+#
+#     def train_world_all(
+#             self,
+#             states: torch.Tensor,
+#             actions: torch.Tensor,
+#             next_states: torch.Tensor,
+#     ) -> None:
+#         """
+#         Train the dynamic of world model.
+#         :param states:
+#         :param actions:
+#         :param next_states:
+#         """
+#         y_y = next_states.T
+#         x_x = torch.cat((states, actions), dim=1)
+#         gpr = gp.models.GPRegression(x_x, y_y, self.kernel, noise=torch.tensor(self.noise), jitter=0.0001).to(self.device)
+#         optimizer = torch.optim.Adam(gpr.parameters(), lr=self.l_r)
+#         loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
+#         for _ in range(self.train_iter):
+#             optimizer.zero_grad()
+#             loss = loss_fn(gpr.model, gpr.guide)
+#             loss.backward()
+#             optimizer.step()
+#         self.gpr = gpr
+#
+#     def pred_next_states(
+#             self, observation: torch.Tensor, actions: torch.Tensor
+#     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+#         """
+#         :param observation:
+#         :param actions:
+#         """
+#         # sample = 3
+#         # preds = []
+#         x = torch.cat((observation, actions), dim=1)
+#         preds, _ = self.gpr(x, full_cov=True)
+#         preds = preds.T
+#         preds += observation
+#         # preds = torch.mean(preds, dim=0).unsqueeze(dim=0)
+#         return preds, None, None, None
+#
+#     def estimate_uncertainty(
+#             self, observation: torch.Tensor, actions: torch.Tensor
+#     ) -> tuple[float, float]:
+#         """
+#         Estimate next state uncertainty and reward uncertainty.
+#
+#         :param observation:
+#         :param actions:
+#         :return:
+#         """
+#         x = torch.cat((observation, actions), dim=1)
+#         _, covs = self.gpr(x, full_cov=True)
+#         uncet = torch.sum(torch.squeeze(covs)).detach().cpu().numpy()
+#         return uncet, 0.0
+#
+#     def train_reward(
+#             self,
+#             states: torch.Tensor,
+#             actions: torch.Tensor,
+#             next_states: torch.Tensor,
+#             rewards: torch.Tensor,
+#     ) -> None:
+#         """
+#         Train the reward prediction with or without world model dynamics.
+#
+#         :param states:
+#         :param actions:
+#         :param next_states:
+#         :param rewards:
+#         """
+#         self.reward_optimizers.zero_grad()
+#         rwd_mean, rwd_var = self.reward_model.forward(states, actions, next_states)
+#         reward_loss = F.gaussian_nll_loss(input=rwd_mean, target=rewards, var=rwd_var).mean()
+#         reward_loss.backward()
+#         self.reward_optimizers.step()
+#
+#     def pred_rewards(self, observation: torch.Tensor, action: torch.Tensor, next_observation: torch.Tensor
+#                      ):
+#         """
+#         Predict reward based on SAS
+#         :param observation:
+#         :param action:
+#         :param next_observation:
+#         :return:
+#         """
+#         pred_reward, reward_var = self.reward_model.forward(observation, action, next_observation)
+#         return pred_reward, None, reward_var
